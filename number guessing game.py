125cb4231b569b902389c2729cffd5f141b54393
welswitcha-marabilis <nathanwhite20133@gmail.com>
2025-11-28 13:52:46 -0600

    Update number guessing game.py

--- number guessing game.py
+++ number guessing game.py
@@ -54,33 +54,5 @@ def check_user_guess(number: int, user_guess: int) -> bool:
     print('higher')
     return False
 
-#================- main -=================#
-
-def main():
-  low = 1
-  high = 100
-  num_of_guesses = 6
-  cur_guesses = 0
-  random_number = get_random_number(low, high)
-
-  while cur_guesses < num_of_guesses:
-    user_guess = None
-    # cycle untill user gives a valid input
-    while user_guess is None:
-      user_guess = get_user_guess(low, high)
-    
-    # win / loss system
-    if check_user_guess(random_number, user_guess):
-      print(f'you got it in {cur_guesses + 1} guesses!')
-      return
-    else:
-      print(f'you have {num_of_guesses - cur_guesses - 1} guesses left.')
-    
-    cur_guesses += 1
-  print(f'you ran out of guesses, the number was {random_number}')
-
-
-if __name__ == '__main__':
-  main()
 
 
